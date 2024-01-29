b6a7ea75fbe213c958010f92c0a6105a3fa004a7
CSwigg <swiggum2@wisc.edu>
2024-01-29 18:47:45 +0100

    forgot to checkout the correct branch :(, but I am mergeing the added sphinx-docs now

--- cluster_traces/cluster_traces.py
+++ cluster_traces/cluster_traces.py
@@ -8,30 +8,77 @@ class StarClusterData:
     """
     Class for storing user input data of a star cluster.
 
-    Attributes:
-    - df (pandas.DataFrame): Input data containing the following columns: x, y, z, U, V, W, name, age_myr.
-    - data_name (str): Name of the star cluster data.
-    - cluster_int_coords (tuple): Integrated coordinates of the star cluster.
-    - coordinates (numpy.ndarray): Array of coordinates (x, y, z, U, V, W) of the star cluster.
-    - integrated (bool): Whether the star cluster has been integrated.
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Input data containing the following columns: x, y, z, U, V, W, name, age_myr.
+    data_name : str
+        Name of the star cluster data.
+    min_size : int, optional
+        Minimum size of the marker points (default is 1).
+    max_size : int, optional
+        Maximum size of the marker points (default is 5).
+    color : str, optional
+        Color of the marker points (default is 'gray').
+    opacity : float, optional
+        Opacity of the marker points (default is 1.0).
 
-    Methods:
-    - __init__(self, df, data_name): Initializes the StarClusterData object.
-    - create_integrated_dataframe(self, time): Creates an integrated DataFrame of the star cluster.
-    - integrate_orbits(self, time): Integrates the orbits of the star cluster.
+    Attributes
+    ----------
+    df : pandas.DataFrame
+        Input data containing the following columns: x, y, z, U, V, W, name, age_myr.
+    data_name : str
+        Name of the star cluster data.
+    cluster_int_coords : tuple
+        Integrated coordinates of the star cluster.
+    coordinates : numpy.ndarray
+        Array of coordinates (x, y, z, U, V, W) of the star cluster.
+    integrated : bool
+        Whether the star cluster has been integrated.
+    color : str
+        Color of the marker points.
+    opacity : float
+        Opacity of the marker points.
+    min_size : int
+        Minimum size of the marker points.
+    max_size : int
+        Maximum size of the marker points.
+
+    Methods
+    -------
+    __init__(self, df, data_name, min_size=1, max_size=5, color='gray', opacity=1.0)
+        Initializes the StarClusterData object.
+    create_integrated_dataframe(self, time)
+        Creates an integrated DataFrame of the star cluster.
+    set_age_based_sizes(self, use_n_stars=False)
+        Set the point sizes for clusters based on the number of stars.
+    integrate_orbits(self, time)
+        Integrates the orbits of the star cluster.
     """
 
-    def __init__(self, df, data_name, min_size = 1, max_size = 5, color = 'gray', opacity = 1.):
+    def __init__(self, df, data_name, min_size=1, max_size=5, color='gray', opacity=1.0):
         """
         Initialize the StarClusterData object.
 
-        Parameters:
-        - df (pandas.DataFrame): Input data containing the following columns: x, y, z, U, V, W, name, age_myr, n_stars.
-        - data_name (str): Name of the star cluster data.
-        - marker_points (dict): Dictionary containing the marker size, color, and opacity.
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            Input data containing the following columns: x, y, z, U, V, W, name, age_myr.
+        data_name : str
+            Name of the star cluster data.
+        min_size : int, optional
+            Minimum size of the marker points (default is 1).
+        max_size : int, optional
+            Maximum size of the marker points (default is 5).
+        color : str, optional
+            Color of the marker points (default is 'gray').
+        opacity : float, optional
+            Opacity of the marker points (default is 1.0).
 
-        Raises:
-        - ValueError: If the input data does not contain the required columns.
+        Raises
+        ------
+        ValueError
+            If the input data does not contain the required columns.
         """
         self.df = df
         self.data_name = data_name
@@ -57,11 +104,15 @@ class StarClusterData:
         """
         Create an integrated DataFrame of the star cluster.
 
-        Parameters:
-        - time (float): The time at which the orbits are integrated.
+        Parameters
+        ----------
+        time : float
+            The time at which the orbits are integrated.
 
-        Returns:
-        - df_int (pandas.DataFrame): Integrated DataFrame of the star cluster.
+        Returns
+        -------
+        df_int : pandas.DataFrame
+            Integrated DataFrame of the star cluster.
         """
         if self.cluster_int_coords is None:
             print('Clusters have not yet been integrated')
@@ -74,23 +125,22 @@ class StarClusterData:
         df_int['time'] = np.tile(time, len(self.df))
         return df_int
     
-    def set_age_based_sizes(self, use_n_stars = False):
+    def set_age_based_sizes(self, use_n_stars=False):
         """
         Set the point sizes for clusters based on the number of stars.
 
-        Parameters:
-        - df_int (pandas.DataFrame): Input dataframe containing cluster data.
-        - nstars_min (int): Minimum number of stars for scaling (default: None).
-        - nstars_max (int): Maximum number of stars for scaling (default: None).
-        - mass_sized (bool): Flag indicating whether to scale based on mass (default: False).
-        - constant_size (int): Constant size for clusters if not scaled (default: 5).
+        Parameters
+        ----------
+        use_n_stars : bool, optional
+            Flag indicating whether to scale based on the number of stars (default is False).
 
-        Returns:
-        - df_int (pandas.DataFrame): Updated dataframe with scaled point sizes.
+        Returns
+        -------
+        df_int : pandas.DataFrame
+            Updated dataframe with scaled point sizes.
         """
-
         df_int = self.df_int
-        df_int_new_sizes = point_sizes.set_cluster_point_sizes(df_int, min_size = self.min_size, max_size = self.max_size)
+        df_int_new_sizes = point_sizes.set_cluster_point_sizes(df_int, min_size=self.min_size, max_size=self.max_size)
         self.df_int = df_int_new_sizes
         
 
@@ -99,8 +149,10 @@ class StarClusterData:
         """
         Integrate the orbits of the star cluster.
 
-        Parameters:
-        - time (float): The time at which the orbits are integrated.
+        Parameters
+        ----------
+        time : float
+            The time at which the orbits are integrated.
         """
         self.cluster_int_coords = orbit_maker.create_orbit(self.coordinates, time)
         self.df_int = self.create_integrated_dataframe(time) # Create the integrated DataFrame
@@ -111,25 +163,36 @@ class StarClusterCollection:
     """
     Class for storing a collection of star clusters.
 
-    Attributes:
-    - clusters (list): List of StarClusterData objects.
+    Attributes
+    ----------
+    clusters : list
+        List of StarClusterData objects.
 
-    Methods:
-    - __init__(self, clusters=[]): Initializes the StarClusterCollection object.
-    - add_cluster(self, cluster): Adds a star cluster to the collection.
-    - get_cluster(self, identifier): Retrieves a star cluster from the collection.
-    - get_all_clusters(self): Retrieves all star clusters from the collection.
+    Methods
+    -------
+    __init__(self, clusters=[])
+        Initializes the StarClusterCollection object.
+    add_cluster(self, cluster)
+        Adds a star cluster to the collection.
+    get_cluster(self, identifier)
+        Retrieves a star cluster from the collection.
+    get_all_clusters(self)
+        Retrieves all star clusters from the collection.
     """
 
     def __init__(self, clusters=[]):
         """
         Initialize the StarClusterCollection object.
 
-        Parameters:
-        - clusters (list): List of StarClusterData objects.
+        Parameters
+        ----------
+        clusters : list, optional
+            List of StarClusterData objects.
 
-        Raises:
-        - ValueError: If any element in clusters is not a StarClusterData object.
+        Raises
+        ------
+        ValueError
+            If any element in clusters is not a StarClusterData object.
         """
         if len(clusters) != 0:
             for cluster in clusters:
@@ -142,11 +205,15 @@ class StarClusterCollection:
         """
         Add a star cluster to the collection.
 
-        Parameters:
-        - cluster (StarClusterData): StarClusterData object to be added.
+        Parameters
+        ----------
+        cluster : StarClusterData
+            StarClusterData object to be added.
 
-        Raises:
-        - ValueError: If cluster is not a StarClusterData object.
+        Raises
+        ------
+        ValueError
+            If cluster is not a StarClusterData object.
         """
         if not isinstance(cluster, StarClusterData):
             raise ValueError('Input must be an instance of StarClusterData')
@@ -156,14 +223,20 @@ class StarClusterCollection:
         """
         Retrieve a star cluster from the collection.
 
-        Parameters:
-        - identifier (str or int): Name or index of the star cluster.
+        Parameters
+        ----------
+        identifier : str or int
+            Name or index of the star cluster.
 
-        Returns:
-        - cluster (StarClusterData): StarClusterData object.
+        Returns
+        -------
+        cluster : StarClusterData
+            StarClusterData object.
 
-        Raises:
-        - ValueError: If identifier is neither a string (name) nor an integer (index).
+        Raises
+        ------
+        ValueError
+            If identifier is neither a string (name) nor an integer (index).
         """
         if isinstance(identifier, str):
             for cluster in self.clusters:
@@ -178,8 +251,10 @@ class StarClusterCollection:
         """
         Retrieve all star clusters from the collection.
 
-        Returns:
-        - clusters (list): List of StarClusterData objects.
+        Returns
+        -------
+        clusters : list
+            List of StarClusterData objects.
         """
         return self.clusters
 
@@ -187,8 +262,10 @@ class StarClusterCollection:
         """
         Integrate the orbits of all star clusters in the collection.
 
-        Parameters:
-        - time (float): The time at which the orbits are integrated.
+        Parameters
+        ----------
+        time : float
+            The time at which the orbits are integrated.
         """
         self.time = time # Makes sure each cluster group is integraetd with the same time array
         for cluster in self.clusters:
