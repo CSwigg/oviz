e9c2d3e5c16bf31d7b476ab4d615743cdc462f67
CSwigg <swiggum2@wisc.edu>
2024-01-29 17:14:15 +0100

    doc-string updates for previous changes

--- cluster_traces/StarClusters3DPlotter.py
+++ cluster_traces/StarClusters3DPlotter.py
@@ -11,97 +11,110 @@ class StarClusters3DPlotter:
 
 
     def default_figure_layout(self):
-        # plotly scene, layout, and camera
-        camera = dict(
-            up=dict(x=0, y=1, z=0),
-            center=dict(x=0, y=0, z=0),
-            eye=dict(x=0., y=-.8, z=1.5),
-            projection = dict(type = 'perspective')
-            ) 
+            """
+            Returns the default layout for the 3D plot figure.
 
-        x_width = 1500
-        y_width = 1500
-        z_width = 600
-        z_aspect = 1.*(z_width/x_width)
+            Returns:
+            layout (plotly.graph_objs.Layout): The default layout for the 3D plot figure.
+            """
+            
+            # plotly scene, layout, and camera
+            camera = dict(
+                up=dict(x=0, y=1, z=0),
+                center=dict(x=0, y=0, z=0),
+                eye=dict(x=0., y=-.8, z=1.5),
+                projection = dict(type = 'perspective')
+                ) 
+
+            x_width = 1500
+            y_width = 1500
+            z_width = 600
+            z_aspect = 1.*(z_width/x_width)
 
 
-        scene = dict(
-                camera = camera,
-                aspectmode = 'manual',
-                aspectratio = dict(x=1, y=1, z=z_aspect),
-                xaxis=dict(
-                    title='X\' (pc)', 
-                    range = [-x_width, x_width],
-                    showgrid=True,
-                    zeroline=False,
-                    showline = True,
-                    mirror = True,
-                    showspikes = False,
-                    nticks = 5,
-                    linecolor = 'white',
-                    linewidth = 2.,
-                    ), 
-                yaxis=dict(
-                    title='Y\' (pc)', 
-                    range = [-y_width, y_width],
-                    showgrid=True,
-                    zeroline=False,
-                    showline = True,
-                    mirror = True,
-                    showspikes = False,
-                    nticks = 5,
-                    linecolor = 'white',
-                    linewidth = 2.,
-                    ), 
-                zaxis=dict(
-                    title='Z\' (pc)', 
-                    range = [-z_width, z_width],
-                    showgrid=True,
-                    zeroline=False,
-                    showline = True,
-                    showspikes = False,
-                    mirror = True,
-                    nticks = 5,
-                    linecolor = 'white',
-                    linewidth = 2.,
-                    ))
-        
-        legend = dict(
-            x = 0,
-            y = 1,
-            title = dict(
-                text = 'Click to toggle traces on/off',
+            scene = dict(
+                    camera = camera,
+                    aspectmode = 'manual',
+                    aspectratio = dict(x=1, y=1, z=z_aspect),
+                    xaxis=dict(
+                        title='X\' (pc)', 
+                        range = [-x_width, x_width],
+                        showgrid=True,
+                        zeroline=False,
+                        showline = True,
+                        mirror = True,
+                        showspikes = False,
+                        nticks = 5,
+                        linecolor = 'white',
+                        linewidth = 2.,
+                        ), 
+                    yaxis=dict(
+                        title='Y\' (pc)', 
+                        range = [-y_width, y_width],
+                        showgrid=True,
+                        zeroline=False,
+                        showline = True,
+                        mirror = True,
+                        showspikes = False,
+                        nticks = 5,
+                        linecolor = 'white',
+                        linewidth = 2.,
+                        ), 
+                    zaxis=dict(
+                        title='Z\' (pc)', 
+                        range = [-z_width, z_width],
+                        showgrid=True,
+                        zeroline=False,
+                        showline = True,
+                        showspikes = False,
+                        mirror = True,
+                        nticks = 5,
+                        linecolor = 'white',
+                        linewidth = 2.,
+                        ))
+            
+            legend = dict(
+                x = 0,
+                y = 1,
+                title = dict(
+                    text = 'Click to toggle traces on/off',
+                    font = dict(
+                        size = 18,  
+                        color = 'white'
+                    ),  
+                    side = 'top'
+                ),
                 font = dict(
-                    size = 18,  
+                    size = 14,
+                    family = 'Arial',
                     color = 'white'
-                ),  
-                side = 'top'
-            ),
-            font = dict(
-                size = 14,
-                family = 'Arial',
-                color = 'white'
-            ),
-            itemsizing = 'constant',
-            bgcolor = 'rgba(0,0,0,0)' # transparent legend
-        )
-
-        layout = go.Layout(
-            scene = scene,
-            template = 'plotly_dark', 
-            dragmode = 'turntable',
-            # width = 1000,
-            # height = 1000,
-            margin = dict(
-                r=10, l=10,
-                b=10, t=10),
-            legend = legend
+                ),
+                itemsizing = 'constant',
+                bgcolor = 'rgba(0,0,0,0)' # transparent legend
             )
-        return layout
+
+            layout = go.Layout(
+                scene = scene,
+                template = 'plotly_dark', 
+                dragmode = 'turntable',
+                # width = 1000,
+                # height = 1000,
+                margin = dict(
+                    r=10, l=10,
+                    b=10, t=10),
+                legend = legend
+                )
+            return layout
 
 
 
     def generate_slider(self):
+        """
+        Generates a slider for controlling the animation of the plot.
+
+        Returns:
+        list: A list containing the slider configuration.
+        """
         sliders = [
             dict(
                 active = len(self.time) - 1,
@@ -146,71 +159,81 @@ class StarClusters3DPlotter:
 
 
 
-    def generate_3d_plot(self, collection, figure_layout = None, show = True, save = False, save_name = None):
-        
+    def generate_3d_plot(self, collection, figure_layout=None, show=True, save=False, save_name=None):
+            """
+            Generates a 3D plot of star clusters over time.
 
+            Parameters:
+            - collection: StarClusterCollection object containing the star clusters to be plotted.
+            - figure_layout: Optional parameter specifying the layout of the plot. If not provided, a default layout will be used.
+            - show: Boolean indicating whether to display the plot.
+            - save: Boolean indicating whether to save the plot as an HTML file.
+            - save_name: Optional parameter specifying the name of the HTML file to save.
 
-        if collection.time is None: # handles if star clusters haven't been integrated yet
-            print('Star clusters have not yet been integrated \n Integrating backwards 60 Myr with 1 Myr time steps...')
-            collection.integrate_clusters(np.arange(0,-60,-1))
+            Returns:
+            None
+            """
+            
+            if collection.time is None: # handles if star clusters haven't been integrated yet
+                print('Star clusters have not yet been integrated \n Integrating backwards 60 Myr with 1 Myr time steps...')
+                collection.integrate_clusters(np.arange(0,-60,-1))
 
-        collection.set_all_cluster_sizes()
+            collection.set_all_cluster_sizes()
 
-        self.time = collection.time 
-        if figure_layout is None:
-            self.figure_layout = self.default_figure_layout()
-        
+            self.time = collection.time 
+            if figure_layout is None:
+                self.figure_layout = self.default_figure_layout()
+            
 
-        '''
-        The following nested loops are strucutred such that for each step in time,
-        every star cluster across the entire collection is plotted.
-        '''
-        cluster_groups = collection.get_all_clusters()
-        fig = {}
-        frames = []
-        for t in self.time:
-            scatter_list = []
-            for cluster_group in cluster_groups:
-                assert cluster_group.integrated == True # make sure that the integrated DataFrame is populated
-                frame = {'data': [], 'name': str(t)}
-                df_int = cluster_group.df_int
-                df_t = df_int[df_int['time'] == t]
-                scatter_cluster_group_t = go.Scatter3d(
-                    x=df_t['x'],
-                    y=df_t['y'],
-                    z=df_t['z'],
-                    mode='markers',
-                    marker = dict(
-                        size = df_t['size'],
-                        color = cluster_group.color,
-                        opacity = cluster_group.opacity,
-                        line = dict(
-                            color = 'black',
-                            width = 0.0
-                        )
-                    ),
-                    hovertext = df_t['name'],
-                    name=cluster_group.data_name
-                ) 
-                scatter_list.append(scatter_cluster_group_t)
-            frame['data'] = scatter_list 
-            frames.append(go.Frame(frame))
+            '''
+            The following nested loops are strucutred such that for each step in time,
+            every star cluster across the entire collection is plotted.
+            '''
+            cluster_groups = collection.get_all_clusters()
+            fig = {}
+            frames = []
+            for t in self.time:
+                scatter_list = []
+                for cluster_group in cluster_groups:
+                    assert cluster_group.integrated == True # make sure that the integrated DataFrame is populated
+                    frame = {'data': [], 'name': str(t)}
+                    df_int = cluster_group.df_int
+                    df_t = df_int[df_int['time'] == t]
+                    scatter_cluster_group_t = go.Scatter3d(
+                        x=df_t['x'],
+                        y=df_t['y'],
+                        z=df_t['z'],
+                        mode='markers',
+                        marker = dict(
+                            size = df_t['size'],
+                            color = cluster_group.color,
+                            opacity = cluster_group.opacity,
+                            line = dict(
+                                color = 'black',
+                                width = 0.0
+                            )
+                        ),
+                        hovertext = df_t['name'],
+                        name=cluster_group.data_name
+                    ) 
+                    scatter_list.append(scatter_cluster_group_t)
+                frame['data'] = scatter_list 
+                frames.append(go.Frame(frame))
 
-        fig['data'] = frames[0]['data']
-        fig['layout'] = self.figure_layout
+            fig['data'] = frames[0]['data']
+            fig['layout'] = self.figure_layout
 
-        # Generate slider
-        slider = self.generate_slider()
-        fig['layout']['sliders'] = slider
+            # Generate slider
+            slider = self.generate_slider()
+            fig['layout']['sliders'] = slider
 
-        fig['frames'] = frames
+            fig['frames'] = frames
 
-        self.figure = go.Figure(fig)
-        if show:
-            self.figure.show()
-        if save:
-            self.figure.write_html(save_name, auto_play = False)
-        #return self.figure
+            self.figure = go.Figure(fig)
+            if show:
+                self.figure.show()
+            if save:
+                self.figure.write_html(save_name, auto_play = False)
         
 
 
