fa4c6143605b1d7019cc405b9a711a0e75889ef9
Cameren Swiggum <44476901+CSwigg@users.noreply.github.com>
2024-01-30 18:15:43 +0100

    Revert "added package files"

---
+++ dash_gaia_orbits/point_sizes.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+def set_cluster_point_sizes(df_int, min_size, max_size):
+
+    if len(df_int) == 0:
+        return df_int
+
+    def compute_size(group):
+        age = group['age_myr'].mean()
+        # size = np.where(group['time'] >= -age, initial_size, min_size)
+        # size = np.where((group['time'] < -age) & (group['time'] >= -age - 3), size * np.exp(group['time'] + age), size)
+
+        size_small = min_size
+        size_large = max_size
+
+        group['size'] = size_large
+        group.loc[group['time'] < -age, 'size'] = size_small
+
+
+        return group
+
+    df_int = df_int.groupby('name').apply(compute_size).sort_index()
+    return df_int
---
+++ docs/build/html/_sources/dash_test_3d_orbits.rst.txt
@@ -0,0 +1,7 @@
+dash\_test\_3d\_orbits module
+=============================
+
+.. automodule:: dash_test_3d_orbits
+   :members:
+   :undoc-members:
+   :show-inheritance:
