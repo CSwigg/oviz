b70f28a363e7c45ba698736432e3ab040bfcb5d4
CSwigg <swiggum2@wisc.edu>
2024-03-13 14:14:18 +0100

    working dropdown

--- dash_gaia_orbits/StarClusters3DPlotter.py
+++ dash_gaia_orbits/StarClusters3DPlotter.py
@@ -11,7 +11,7 @@ class StarClusters3DPlotter:
     """
 
 
-    def __init__(self, data_collection, xyz_widths = (1000, 1000, 300), figure_theme = None, plotly_light_template = None, figure_layout = None, figure_layout_dict = None):
+    def __init__(self, data_collection, xyz_widths = (1000, 1000, 300), figure_theme = None, plotly_light_template = None, figure_layout = None, figure_layout_dict = None, trace_grouping_dict = None):
         """
         Initializes a StarClusters3DPlotter object.
 
@@ -31,9 +31,15 @@ class StarClusters3DPlotter:
         self.xyz_widths = xyz_widths
         self.figure_theme = figure_theme
         self.plot_light_template = plotly_light_template
+        self.trace_grouping_dict = trace_grouping_dict
 
         self.figure_layout_dict = read_theme(self)
 
+
+        if self.trace_grouping_dict is None:
+            self.trace_grouping_dict = {}
+        # prepend All to self.trace_grouping_dict
+        self.trace_grouping_dict = {'All': [cluster.data_name for cluster in self.data_collection.get_all_clusters()], **self.trace_grouping_dict}
 
     
     def generate_slider(self):
@@ -105,8 +111,37 @@ class StarClusters3DPlotter:
         else:
             return None
 
+    def get_visibility(self, traces_list):
+        visibility = []
+        for trace in self.fig['data']:
+            if trace['name'] in traces_list:
+                visibility.append(True)
+            elif trace['name'] in self.static_trace_names:
+                visibility.append(False)
+            else:
+                visibility.append(False)
+        return visibility
 
-
+    def dropdown_menu(self):
+        buttons = []
+        for key, traces_list in self.trace_grouping_dict.items():
+            visibility = self.get_visibility(traces_list)
+            buttons.append(dict(
+                args=[{'visible': visibility}],
+                label=key,
+                method='update'
+            ))
+        dropdown = [dict(
+            buttons=buttons,
+            direction='down',
+            pad={'r': 10, 't': 10},
+            showactive=True,
+            x=0,
+            xanchor='left',
+            y=1.1,
+            yanchor='top'
+        )]
+        return dropdown
 
 
     def generate_3d_plot(self, time = None, figure_layout=None, show=False, save_name=None, static_traces = None, static_traces_times = None, reference_frame_center = None, focus_group = None, fade_in_time = 5):
@@ -132,15 +167,13 @@ class StarClusters3DPlotter:
 
         self.time = self.data_collection.time 
         self.figure_layout = go.Layout(self.figure_layout_dict)
-
-        
-
+        self.all_traces = []
         '''
         The following nested loops are strucutred such that for each step in time,
         every star cluster across the entire collection is plotted.
         '''
         cluster_groups = self.data_collection.get_all_clusters()
-        fig = {}
+        self.fig = {}
         frames = []
         for t in self.time:
             scatter_list = []
@@ -174,30 +207,34 @@ class StarClusters3DPlotter:
                 scatter_list.append(scatter_cluster_group_t)
             frame['data'] = scatter_list 
 
+            self.static_trace_names = []
             if static_traces is not None and static_traces_times is not None:
                 for i, st in enumerate(static_traces):
+                    self.static_trace_names.append(st['name'])
                     if reference_frame_center is not None:
                         st['x'] = st['x'] - reference_frame_center[0]
                         st['y'] = st['y'] - reference_frame_center[1]
                         st['z'] = st['z'] - reference_frame_center[2]
-                    if t in static_traces_times[i]:
+                    if t == 0:
                         visible = True
                     else:
                         visible = False
-                        # place holder
-                        st = go.Scatter3d()
+                        st = go.Scatter3d() # place holder
                     st['visible'] = visible
                     frame['data'].append(st)
             frames.append(go.Frame(frame))
             
-        fig['data'] = frames[0]['data']
-        fig['layout'] = self.figure_layout
-        # Generate slider
+        self.fig['data'] = frames[0]['data']
+        self.fig['layout'] = self.figure_layout
         slider = self.generate_slider()
-        fig['layout']['sliders'] = slider
-        fig['frames'] = frames
-        self.fig_dict = fig
-        self.figure = go.Figure(fig)
+        self.fig['layout']['sliders'] = slider
+        self.fig['frames'] = frames
+
+        # dropdown menu for selecting which traces to display
+        dropdown = self.dropdown_menu()
+        self.fig['layout']['updatemenus'] = dropdown
+        self.fig_dict = self.fig
+        self.figure = go.Figure(self.fig)
         if show:
             self.figure.show()
         if save_name is not None:
